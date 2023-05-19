489ecd0f2832b9e47173d4459b5c4e04581fd7d6
Lucas Matos <lucaslmatos18@gmail.com>
2023-05-18 23:30:01 -0300

    REQ 02 - Criado endpoint /talker/:id

--- src/index.js
+++ src/index.js
@@ -13,15 +13,26 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (req, res) => {
+const getData = async () => {
   const path = '/talker.json';
-  try {
-    const content = await fs.readFile(join(__dirname, path), 'utf-8');
-    const data = JSON.parse(content);
+  const content = await fs.readFile(join(__dirname, path), 'utf-8');
+  const data = JSON.parse(content);
+  return data;
+};
+
+app.get('/talker', async (req, res) => {
+  const data = await getData();
     return res.status(200).json(data) || res.status(200).json([]);
-  } catch (e) {
-    return null;
-  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await getData();
+  const talker = data.find((tk) => tk.id === +id);
+  if (talker) {
+    return res.status(200).json(talker); 
+  } 
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.listen(PORT, () => {
